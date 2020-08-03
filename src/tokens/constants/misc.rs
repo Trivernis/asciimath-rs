a966b38f33a1851344d69d2710739afd58f7236b
trivernis <trivernis@protonmail.com>
2020-08-03 09:36:00 +0200

    Add misc symbols

---
+++ src/tokens/constants/misc.rs
@@ -0,0 +1,40 @@
+pub const A_FRAC: &str = "/";
+pub const T_FRAC: &str = "frac";
+
+pub const G_POW: &'static[&str] = &["^"];
+pub const G_SQRT: &'static[&str] = &["sqrt"];
+pub const G_ROOT: &'static[&str] = &["root"];
+pub const G_INT: &'static[&str] = &["int"];
+pub const G_OINT: &'static[&str] = &["oint"];
+pub const G_DEL: &'static[&str] = &["del", "partial"];
+pub const G_GRAD: &'static[&str] = &["grad", "nbla"];
+pub const G_PM: &'static[&str] = &["+-", "pm"];
+pub const G_EMPTYSET: &'static[&str] = &["O/", "emptyset"];
+pub const GINFTY: &'static[&str] = &["oo", "infty"];
+pub const G_ALEPH: &'static[&str] = &["aleph"];
+pub const G_THEREFORE: &'static[&str] = &[":.", "therefore"];
+pub const G_BECAUSE: &'static[&str] = &[":'", "because"];
+pub const G_ELDOTS: &'static[&str] = &["|...|", "|ldots|"];
+pub const G_ECDOTS: &'static[&str] = &["|cdots|"];
+pub const G_VDOTs: &'static[&str] = &["vdots"];
+pub const G_DDOTS: &'static[&str] = &["ddots"];
+pub const G_EPIPES: &'static[&str] = &["|\\ |"];
+pub const G_QUAD: &'static[&str] = &["|quad|"];
+pub const G_ANGLE: &'static[&str] = &["/_", "angle"];
+pub const G_FROWN: &'static[&str] = &["frown"];
+pub const G_TRIANGLE: &'static[&str] = &["/_\\", "triangle"];
+pub const G_DIAMOND: &'static[&str] = &["diamond"];
+pub const G_SQUARE: &'static[&str] = &["square"];
+pub const G_LFLOOR: &'static[&str] = &["|__", "lfloor"];
+pub const G_RFLOOR: &'static[&str] = &["__|", "rfloor"];
+pub const G_LCEILING: &'static[&str] = &["|~", "lceiling"];
+pub const G_RCEILING: &'static[&str] = &["~|", "rceiling"];
+
+pub const G_COMPLEX: &'static[&str] = &["CC"];
+pub const G_NATURAL: &'static[&str] = &["NN"];
+pub const G_RATIONAL: &'static[&str] = &["QQ"];
+pub const G_REAL: &'static[&str] = &["RR"];
+pub const G_INTEGER: &'static[&str] = &["ZZ"];
+
+pub const A_TEXT: &str = "\"";
+pub const T_TEX: &str = "text";
