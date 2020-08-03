fbe87fa4331179ac64a18a2af0b7bfa768f9ceac
trivernis <trivernis@protonmail.com>
2020-08-03 10:02:46 +0200

    Add logical symbols

---
+++ src/tokens/constants/logical.rs
@@ -0,0 +1,12 @@
+pub const G_AND: &'static[&str] = &["and"];
+pub const G_OR: &'static[&str] = &["or"];
+pub const G_NOT: &'static[&str] = &["not", "neg"];
+pub const G_IMPLIES: &'static[&str] = &["=>", "implies"];
+pub const G_IF: &'static[&str] = &["if"];
+pub const G_IFF: &'static[&str] = &["<=>", "iff"];
+pub const G_FORALL: &'static[&str] = &["AA", "forall"];
+pub const G_EXISTS: &'static[&str] = &["EE", "exists"];
+pub const G_BOT: &'static[&str] = &["_|_", "bot"];
+pub const G_TOP: &'static[&str] = &["TT", "top"];
+pub const G_VDASH: &'static[&str] = &["|--", "vdash"];
+pub const G_MODELS: &'static[&str] = &["|==", "models"];
--- src/tokens/constants/mod.rs
+++ src/tokens/constants/mod.rs
@@ -1,3 +1,4 @@
 pub mod operations;
 pub mod misc;
 pub mod relation;
+pub mod logical;
